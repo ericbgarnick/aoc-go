f7732c5b60775e7ab1318d3583367338f53e18d4
Eric Garnick <eric@measured.fi>
2024-12-05 09:41:05 -0500

    Day 4 part 1

---
+++ y2024/day04/day04.go
@@ -0,0 +1,181 @@
+package day04
+
+import (
+	"fmt"
+
+	"github.com/ericbgarnick/aoc-go/util"
+)
+
+const fileName = "y2024/day04/data.txt"
+
+// Part1 counts the occurrences of the string "XMAS" in the grid, going in any direction.
+func Part1() {
+	grid := util.ScanFile(fileName)
+	word := "XMAS"
+	fmt.Printf("Part 1: %d\n", countOccurrences(grid, word))
+}
+
+// Part2 counts the occurrences in the grid of the string "MAS" crossing diagonally on "A".
+func Part2() {
+	grid := util.ScanFile(fileName)
+	word := "MAS"
+	fmt.Printf("Part 2: %d\n", countCrosses(grid, word))
+}
+
+type position struct {
+	row, col int
+}
+
+func countOccurrences(grid []string, word string) int {
+	var total int
+	for r, row := range grid {
+		for c := range row {
+			if row[c] == word[0] {
+				p := position{row: r, col: c}
+				if searchNorth(grid, word, p) {
+					total++
+				}
+				if searchSouth(grid, word, p) {
+					total++
+				}
+				if searchEast(grid, word, p) {
+					total++
+				}
+				if searchWest(grid, word, p) {
+					total++
+				}
+				if searchNortheast(grid, word, p) {
+					total++
+				}
+				if searchNorthwest(grid, word, p) {
+					total++
+				}
+				if searchSoutheast(grid, word, p) {
+					total++
+				}
+				if searchSouthwest(grid, word, p) {
+					total++
+				}
+			}
+		}
+	}
+	return total
+}
+
+func countCrosses(grid []string, word string) int {
+	var total int
+	// TODO: implement
+	return total
+}
+
+func searchNorth(grid []string, word string, start position) bool {
+	if len(word)-start.row > 1 {
+		return false
+	}
+	var curLetter int
+	for curLetter < len(word) {
+		if grid[start.row-curLetter][start.col] != word[curLetter] {
+			return false
+		}
+		curLetter++
+	}
+	return true
+}
+
+func searchSouth(grid []string, word string, start position) bool {
+	if start.row+len(word) > len(grid) {
+		return false
+	}
+	var curLetter int
+	for curLetter < len(word) {
+		if grid[start.row+curLetter][start.col] != word[curLetter] {
+			return false
+		}
+		curLetter++
+	}
+	return true
+}
+
+func searchEast(grid []string, word string, start position) bool {
+	if start.col+len(word) > len(grid[0]) {
+		return false
+	}
+	var curLetter int
+	for curLetter < len(word) {
+		if grid[start.row][start.col+curLetter] != word[curLetter] {
+			return false
+		}
+		curLetter++
+	}
+	return true
+}
+
+func searchWest(grid []string, word string, start position) bool {
+	if len(word)-start.col > 1 {
+		return false
+	}
+	var curLetter int
+	for curLetter < len(word) {
+		if grid[start.row][start.col-curLetter] != word[curLetter] {
+			return false
+		}
+		curLetter++
+	}
+	return true
+}
+
+func searchNortheast(grid []string, word string, start position) bool {
+	if len(word)-start.row > 1 || start.col+len(word) > len(grid[0]) {
+		return false
+	}
+	var curLetter int
+	for curLetter < len(word) {
+		if grid[start.row-curLetter][start.col+curLetter] != word[curLetter] {
+			return false
+		}
+		curLetter++
+	}
+	return true
+}
+
+func searchNorthwest(grid []string, word string, start position) bool {
+	if len(word)-start.row > 1 || len(word)-start.col > 1 {
+		return false
+	}
+	var curLetter int
+	for curLetter < len(word) {
+		if grid[start.row-curLetter][start.col-curLetter] != word[curLetter] {
+			return false
+		}
+		curLetter++
+	}
+	return true
+}
+
+func searchSoutheast(grid []string, word string, start position) bool {
+	if start.row+len(word) > len(grid) || start.col+len(word) > len(grid[0]) {
+		return false
+	}
+	var curLetter int
+	for curLetter < len(word) {
+		if grid[start.row+curLetter][start.col+curLetter] != word[curLetter] {
+			return false
+		}
+		curLetter++
+	}
+	return true
+}
+
+func searchSouthwest(grid []string, word string, start position) bool {
+	if start.row+len(word) > len(grid) || len(word)-start.col > 1 {
+		return false
+	}
+	var curLetter int
+	for curLetter < len(word) {
+		if grid[start.row+curLetter][start.col-curLetter] != word[curLetter] {
+			return false
+		}
+		curLetter++
+	}
+	return true
+}
--- y2024/run.go
+++ y2024/run.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ericbgarnick/aoc-go/y2024/day01"
 	"github.com/ericbgarnick/aoc-go/y2024/day02"
 	"github.com/ericbgarnick/aoc-go/y2024/day03"
+	"github.com/ericbgarnick/aoc-go/y2024/day04"
 )
 
 func Run(dayNum int) {
@@ -22,6 +23,10 @@ func Run(dayNum int) {
 		fmt.Println("Day 3")
 		day03.Part1()
 		day03.Part2()
+	case 4:
+		fmt.Println("Day 4")
+		day04.Part1()
+		day04.Part2()
 	default:
 		fmt.Printf("No solution for day %d\n", dayNum)
 	}
