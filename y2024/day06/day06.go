9204c322b52b0080544918ff3c71d5296f9c0bec
Eric Garnick <eric.b.garnick@gmail.com>
2024-12-08 08:56:30 -0500

    day 6 part 2

--- y2024/day06/day06.go
+++ y2024/day06/day06.go
@@ -2,7 +2,6 @@ package day06
 
 import (
 	"fmt"
-
 	"github.com/ericbgarnick/aoc-go/util"
 )
 
@@ -15,27 +14,70 @@ type position struct {
 	row, col int
 }
 
+type history map[position]map[rune]bool
+
 type guard struct {
 	*position
 	direction rune
+	startPos  position
+	history
+}
+
+func newGuard(p *position, d rune) *guard {
+	g := guard{
+		position:  p,
+		direction: d,
+		history:   history{},
+	}
+	g.startPos = *g.position
+	return &g
 }
 
 func Part1() {
-	var history = make(map[position]bool)
 	floorMap := util.ScanAOCDataFile(2024, 6)
-	g := guard{
-		position:  findGuard(floorMap),
-		direction: guardStart,
-	}
-	for isInBounds(floorMap, g.position) {
-		history[*g.position] = true
-		g = moveGuard(floorMap, g)
-	}
-	fmt.Printf("Part 1: %d\n", len(history))
+	g := newGuard(findGuard(floorMap), guardStart)
+	g, _ = simulate(floorMap, g)
+	fmt.Printf("Part 1: %d\n", len(g.history))
 }
 
 func Part2() {
-	fmt.Println("Part 2: TODO")
+	var (
+		loops   = make(map[position]bool)
+		hasLoop bool
+	)
+	floorMap := util.ScanAOCDataFile(2024, 6)
+	g := newGuard(findGuard(floorMap), guardStart)
+	g, _ = simulate(floorMap, g)
+	for p, dirs := range g.history {
+		for d := range dirs {
+			if !(p == g.startPos && d == guardStart) {
+				newFloor, obstacleP := addObstacle(floorMap, &p, d)
+				if newFloor != nil {
+					if _, hasLoop = simulate(newFloor, newGuard(&g.startPos, guardStart)); hasLoop {
+						loops[*obstacleP] = true
+					}
+				}
+			}
+		}
+	}
+	fmt.Printf("Part 2: %d\n", len(loops))
+}
+
+func simulate(floorMap []string, g *guard) (*guard, bool) {
+	for isInBounds(floorMap, g.position) {
+		prevDirections, ok := g.history[*g.position]
+		if prevDirections[g.direction] {
+			return g, true
+		}
+		if ok {
+			g.history[*g.position][g.direction] = true
+		} else {
+			g.history[*g.position] = map[rune]bool{g.direction: true}
+		}
+
+		g = moveGuard(floorMap, g)
+	}
+	return g, false
 }
 
 func findGuard(floorMap []string) *position {
@@ -53,40 +95,82 @@ func isInBounds(floorMap []string, p *position) bool {
 	return p.row >= 0 && p.col >= 0 && p.row < len(floorMap) && p.col < len(floorMap[0])
 }
 
-func moveGuard(floorMap []string, g guard) guard {
-	curP := g.position
-	nextP := g.position
+func moveGuard(floorMap []string, g *guard) *guard {
 	nextD := g.direction
 
-	switch g.direction {
+	nextP := getNextPosition(floorMap, g.position, g.direction)
+	if nextP == g.position {
+		nextD = turn(g.direction)
+	}
+
+	return &guard{
+		position:  nextP,
+		direction: nextD,
+		startPos:  g.startPos,
+		history:   g.history,
+	}
+}
+
+func getNextPosition(floorMap []string, p *position, d rune) *position {
+	curP := p
+	nextP := p
+
+	switch d {
 	case '^':
 		nextP = &position{row: curP.row - 1, col: curP.col}
 		if isInBounds(floorMap, nextP) && floorMap[nextP.row][nextP.col] == obstacle {
-			nextP = g.position
-			nextD = '>'
+			nextP = p
 		}
 	case '>':
 		nextP = &position{row: curP.row, col: curP.col + 1}
 		if isInBounds(floorMap, nextP) && floorMap[nextP.row][nextP.col] == obstacle {
-			nextP = g.position
-			nextD = 'v'
+			nextP = p
 		}
 	case 'v':
 		nextP = &position{row: curP.row + 1, col: curP.col}
 		if isInBounds(floorMap, nextP) && floorMap[nextP.row][nextP.col] == obstacle {
-			nextP = g.position
-			nextD = '<'
+			nextP = p
 		}
 	case '<':
 		nextP = &position{row: curP.row, col: curP.col - 1}
 		if isInBounds(floorMap, nextP) && floorMap[nextP.row][nextP.col] == obstacle {
-			nextP = g.position
-			nextD = '^'
+			nextP = p
 		}
 	}
+	return nextP
+}
 
-	return guard{
-		position:  nextP,
-		direction: nextD,
+func turn(direction rune) rune {
+	switch direction {
+	case '^':
+		return '>'
+	case '>':
+		return 'v'
+	case 'v':
+		return '<'
+	case '<':
+		return '^'
+	}
+	panic(fmt.Sprintf("unknown position %s", string(direction)))
+}
+
+func addObstacle(floorMap []string, p *position, d rune) ([]string, *position) {
+	nextP := getNextPosition(floorMap, p, d)
+	if !isInBounds(floorMap, nextP) {
+		return nil, nil
+	}
+	var newFloor []string
+	for r, row := range floorMap {
+		if r == nextP.row {
+			row = row[:nextP.col] + string(obstacle) + row[nextP.col+1:]
+		}
+		newFloor = append(newFloor, row)
+	}
+	return newFloor, nextP
+}
+
+func printFloor(floorMap []string) {
+	for _, row := range floorMap {
+		fmt.Println(row)
 	}
 }
