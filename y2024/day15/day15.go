e4521d0027058b91aca02ca555efec1978cb2d90
Eric Garnick <eric@measured.fi>
2024-12-31 18:57:00 -0500

    WIP move too far

--- y2024/day15/day15.go
+++ y2024/day15/day15.go
@@ -11,3 +11,94 @@ func Part1() {
 func Part2() {
 	fmt.Printf("Part 2: %s\n", "TODO")
 }
+
+const (
+	Box   = 'O'
+	Wall  = '#'
+	Robot = '@'
+	Floor = '.'
+)
+
+type Position struct {
+	row, col int
+}
+
+func NewPosition(row, col int) *Position {
+	return &Position{row: row, col: col}
+}
+
+type Warehouse struct {
+	numRows, numCols int
+	floorPlan        [][]rune
+}
+
+func NewWarehouse(rawFloorPlan []string) *Warehouse {
+	wh := Warehouse{
+		numRows: len(rawFloorPlan),
+		numCols: len(rawFloorPlan[0]),
+	}
+	for _, row := range rawFloorPlan {
+		wh.floorPlan = append(wh.floorPlan, []rune(row))
+	}
+	return &wh
+}
+
+func (wh *Warehouse) GetFloorPlan() [][]rune {
+	return wh.floorPlan
+}
+
+func (wh *Warehouse) Move(robotP *Position, d rune) {
+	// sanity-check we are starting with a robot
+	if wh.floorPlan[robotP.row][robotP.col] != Robot {
+		panic(fmt.Sprintf(
+			"non-robot start for shift boxes: %c at %v",
+			wh.floorPlan[robotP.row][robotP.col], robotP),
+		)
+	}
+
+	// handle non-box spaces
+	nextP := NextPosition(robotP, d)
+	if nextObject := wh.floorPlan[nextP.row][nextP.col]; nextObject == Wall {
+		return
+	} else if nextObject == Floor {
+		wh.floorPlan[robotP.row][robotP.col] = Floor
+		wh.floorPlan[nextP.row][nextP.col] = Robot
+		return
+	}
+
+	// traverse boxes
+	for wh.floorPlan[nextP.row][nextP.col] == Box {
+		nextP = NextPosition(nextP, d)
+	}
+
+	// traverse floor spaces, adding boxes
+	var numFloorTiles int
+	for wh.floorPlan[nextP.row][nextP.col] == Floor {
+		numFloorTiles++
+		wh.floorPlan[nextP.row][nextP.col] = Box
+		nextP = NextPosition(nextP, d)
+	}
+
+	// clean up moved boxes and move robot
+	wh.floorPlan[robotP.row][robotP.col] = Floor
+	nextP = NextPosition(robotP, d)
+	for i := 1; i < numFloorTiles; i++ {
+		wh.floorPlan[nextP.row][nextP.col] = Floor
+		nextP = NextPosition(nextP, d)
+	}
+	wh.floorPlan[robotP.row][robotP.col] = Robot
+}
+
+func NextPosition(p *Position, d rune) *Position {
+	if d == '>' {
+		return NewPosition(p.row, p.col+1)
+	} else if d == '<' {
+		return NewPosition(p.row, p.col-1)
+	} else if d == '^' {
+		return NewPosition(p.row-1, p.col)
+	} else if d == 'v' {
+		return NewPosition(p.row+1, p.col)
+	} else {
+		panic(fmt.Sprintf("unknown direction %c", d))
+	}
+}
--- y2024/day15/day15_test.go
+++ y2024/day15/day15_test.go
@@ -1,1 +1,53 @@
 package day15_test
+
+import (
+	"testing"
+
+	"github.com/ericbgarnick/aoc-go/y2024/day15"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNextPosition(t *testing.T) {
+	p := day15.NewPosition(3, 4)
+	tests := map[rune]*day15.Position{
+		'^': day15.NewPosition(2, 4),
+		'v': day15.NewPosition(4, 4),
+		'<': day15.NewPosition(3, 3),
+		'>': day15.NewPosition(3, 5),
+	}
+	for d, wantP := range tests {
+		t.Run(string(d), func(t *testing.T) {
+			gotP := day15.NextPosition(p, d)
+			assert.Equal(t, *wantP, *gotP)
+		})
+	}
+}
+
+func TestWarehouse_Move(t *testing.T) {
+	t.Run("shift robot right", func(t *testing.T) {
+		floopPlan := []string{
+			"#.@.OOO..#",
+		}
+		wh := day15.NewWarehouse(floopPlan)
+		wantFloorPlan := [][]rune{
+			{
+				'#', '.', '.', '@', 'O', 'O', 'O', '.', '.', '#',
+			},
+		}
+		wh.Move(day15.NewPosition(0, 2), '>')
+		assert.Equal(t, wantFloorPlan, wh.GetFloorPlan())
+	})
+	t.Run("shift boxes right", func(t *testing.T) {
+		floopPlan := []string{
+			"#..@OOO..#",
+		}
+		wh := day15.NewWarehouse(floopPlan)
+		wantFloorPlan := [][]rune{
+			{
+				'#', '.', '.', '.', '.', '@', 'O', 'O', 'O', '#',
+			},
+		}
+		wh.Move(day15.NewPosition(0, 2), '>')
+		assert.Equal(t, wantFloorPlan, wh.GetFloorPlan())
+	})
+}
